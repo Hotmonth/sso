7d339780932309ad349cf665dad7e69187d1e408
Hotmonth <ratadatax@gmail.com>
2024-01-07 16:58:44 +0600

    Init commit

---
+++ cmd/sso/main.go
@@ -0,0 +1,11 @@
+package sso
+
+func main() {
+	// TODO: Init config object
+
+	// TODO: Init a logger (slog)
+
+	// TODO: Init app
+
+	// TODO: Run grpc-server app
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module sso
+
+go 1.21.3
+
+require github.com/Hotmonth/protos v0.0.0-20240103172749-7ba7ec36275a // indirect
